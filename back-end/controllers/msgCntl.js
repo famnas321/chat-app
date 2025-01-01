525c4c000d56859a0f39212c33f0c327d2e4de07
famnas321 <famnaspk380@gmail.com>
2025-01-01 13:44:14 +0530

    committed on 1.1.2025

--- back-end/controllers/msgCntl.js
+++ back-end/controllers/msgCntl.js
@@ -30,9 +30,10 @@ if (!conversation) {
   
 if(newMessage){
     conversation.messages.push(newMessage._id)
-    await newMessage.save()
-    await conversation.save()
-}
+    // await newMessage.save()
+    // await conversation.save()
+    Promise.all([newMessage.save(),conversation.save()])
+} 
   res.status(201).json({"message sent successfully":newMessage})
     }
     catch(error){
@@ -41,4 +42,17 @@ if(newMessage){
       return res.status(500).json("unexpected error occured on server while creating conversation")
     }
 
+}
+export const recieveMessage= async (req,res)=>{
+  try{
+    const {id:userToChatId}=req.params
+    const senderId=req.user._id
+    const conversation= Conversation.findOne({
+      participants:{$all:[senderId,userToChatId]}
+    }).populate("messages")
+    res.status(200).json(conversation.message)
+  }catch(error){
+   console.log("Error in getMessageController",error.message)
+   res.status(500).json("unexpected error occured on server")
+  }
 }
--- back-end/routes/message.rout.js
+++ back-end/routes/message.rout.js
@@ -4,5 +4,6 @@ const  router=express.Router()
 import { sendMessage } from "../controllers/msgCntl.js"
 import protectRouts from "../middleware/protect.js"
 router.post("/send/:id",protectRout,sendMessage)
+router.get("/get/:id",protectRout,recieveMessage)
 
 export default router
