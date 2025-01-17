5f714ce65c734ce3247654540d2e9b5a77e8d69d
famnas321 <famnaspk380@gmail.com>
2025-01-17 18:53:48 +0530

    committed on 17.1.2025 /3

--- front-end/src/components/messages/MessageContainer.jsx
+++ front-end/src/components/messages/MessageContainer.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation'
+function MessageContainer({message}) {
 
-function MessageContainer() {
+   const{authUser} =useAuthContext() 
+   const {selectedConversation}= useConversation()
+
+   const fromMe= message.senderId === authUser._id
+   const  chatClassName= fromMe? 'chat-end': 'chat-start'
+   const profilePic= fromMe? authUser.profilePic :selectedConversation?.profilePic
+   const bubbleBgColour=fromMe? 'bg-blue-500' :""
+   console.log(message.message);
   return (
-    <div className='chat chat-end  '>
+    <div className={`${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src="https://cdn-icons-png.flaticon.com/512/147/147144.png" alt="" />
+                <img src={profilePic} alt="" />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi what's up</div>
+        
+        
+        <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColour}`}>{message.message}</div>
         <div className='chat-footer opacity-50 text-xs flex gap-1 items-center '>12:42</div>
     </div>
   )
--- front-end/src/components/messages/Messages.jsx
+++ front-end/src/components/messages/Messages.jsx
@@ -8,15 +8,20 @@ function Messages() {
   
   return (
     <>
-    <div className='px-4 flex-1 h-[500px] '>
-      {loading && [...Array(3)].map((_, idx) => <Skeleton key={idx} />)}
+    <div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} >
+						<MessageContainer message={message} />
+					</div>
+				))}
+
+			{loading && [...Array(3)].map((_, idx) => <Skeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
-				<p className='text-center text-gray-400'>Send a message to start the conversation</p>)}
-
-
-
-
-    </div>
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+		</div>
     </>
   )
 }
