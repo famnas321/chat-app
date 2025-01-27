c02cbd04598926cf51da921f9c37710470ce37fa
famnas321 <famnaspk380@gmail.com>
2025-01-27 17:49:37 +0530

    committed on 27,1,2025 /5

---
+++ front-end/src/context/SocketContext.jsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const SocketContext =createContext()
+
+export const SocketContextProvider =({children})=>{
+    return(
+        <SocketContext.Provider value={{}}>
+            {children}
+        </SocketContext.Provider>
+    )
+
+}
